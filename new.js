c799abb9e71a93b1867323799c8ab55dc6330c47
Clive Chan <cc@clive.io>
2015-10-12 00:34:39 -0400

    lol security kinda works but isn't actually

--- new.js
+++ new.js
@@ -1,9 +1,57 @@
 var baseFirebaseURL = "https://doecaptains.firebaseio.com";
 var currFirebaseURL = baseFirebaseURL + "/lexnsb_2015-2016";
 
-var app = angular.module("DOECaptainsApp", ["firebase"]);
+var app = angular.module("DOECaptainsApp", ["firebase", "ngCookies"]);
 
-app.controller("List", function($scope, $firebaseArray){
+function copyFbRecord(oldRef, newRef) {    
+	oldRef.once('value', function(snap)  {
+	console.log(snap);
+		newRef.set( snap.val(), function(error) {
+			if( error && typeof(console) !== 'undefined' && console.error ) {  console.error(error); }
+		});
+	});
+}
+//var oldRef = new Firebase(baseFirebaseURL+"/"+CryptoJS.SHA3(PASSWORD).toString()+"/lexnsb_2015-2016");var newRef =new Firebase(baseFirebaseURL+"/X"+CryptoJS.SHA3(PASSWORD).toString()+"/lexnsb_2015-2016"); copyFbRecord(oldRef,newRef);
+
+app.controller("IndexLogin", function($scope,$cookies){
+	var fb = new Firebase(baseFirebaseURL);
+	$scope.pwd = "";
+	$scope.authed = null;
+	
+	$scope.login = function(authpath){
+		if(!authpath)
+			var authpath = "X"+CryptoJS.SHA3($scope.pwd);
+		
+		fb.child(authpath).once("value",function(snapshot){
+			$scope.pwd = "";
+			if(snapshot.exists()){
+				$cookies.put("authpath",authpath);
+				$scope.authed = true;
+			}else{
+				$scope.logout();
+				alert("failed login");
+			}
+			$scope.$digest();
+		});
+	};
+	$scope.logout = function(){
+		$cookies.remove("authpath");
+		$scope.authed = false;
+	};
+	$scope.processLoginKeypress = function(e){
+		if(e.keyCode == 13)
+			$scope.login();
+	};
+	
+	if($cookies.get("authpath"))
+		$scope.login($cookies.get("authpath"));
+	else
+		$scope.logout();
+});
+
+app.controller("List", function($scope, $firebaseArray, $cookies){
+	var currFirebaseURL = baseFirebaseURL + "/" + $cookies.get("authpath") + "/lexnsb_2015-2016";
+	
 	$scope.init = function(fbPath,fields,predicate){
 		$scope.predicate = predicate;
 		$scope.direction = true;
@@ -68,4 +116,31 @@ app.controller("List", function($scope, $firebaseArray){
 				return true;
 		return false;
 	}
+});
+app.controller("Members", function($scope, $firebaseArray){
+	//IMPORT
+	/*var str = "Vivek Bhupatiraju <triforce314@gmail.com>, Will Sherwood <whsherwood@gmail.com>, Afareen Jaleel <afareenj@gmail.com>, Aiyappa Kodendera <pandikary@gmail.com>, Andrew Wang <andrewwang86@gmail.com>, Bharat Srirangam <bharatsrirangam@gmail.com>, Cathwin <cricketgottachirp@gmail.com>, Clive Chan <doobahead@gmail.com>, Derik Kauffman <derikk@me.com>, DOE Captains <doecaptains@gmail.com>, Eric Xia <ericzxia@gmail.com>, Evan Fang <fangevan85@gmail.com>, Jason DiMasi <notjasonderulo99@gmail.com>, John Guo <johnguolex@gmail.com>, Karen Zhou <karenz12321@gmail.com>, Nicholas Gould <ngould@sch.ci.lexington.ma.us>, Ravi Raghavan <ravi1998@gmail.com>, Robert Pohlman <rpohlman@sch.ci.lexington.ma.us>, Shivi Maheswaran <shivimaheswaran@gmail.com>, Snigdha Allaparthi <snigy2000@gmail.com>, Sophia Zhang <szhanghdp@gmail.com>, Vishnu Amrit <vishnuamritpydah@gmail.com>";
+	var spl = str.split(", ");
+	var arr = new Array();
+	for(var i = 0; i < spl.length; i++){
+		arr[i]={};
+		arr[i].name=spl[i].split(" <")[0];
+		arr[i].email=spl[i].split(" <")[1].slice(0,-1);
+		$scope.$parent.data.$add(arr[i]);
+	}
+	console.log(arr);*/
+	
+	$scope.$parent.init("/members", [
+		{name:"name",width:"15em",display:"Name"},
+		{name:"email",width:"8em",display:"Email"},
+		{name:"grade",width:"2em",display:"Grade"},
+		{name:"subj1",width:"10em",display:"Subject 1"},
+		{name:"subj2",width:"10em",display:"Subject 2"}
+	], "name");
+});
+app.controller("Rounds", function($scope, $firebaseArray){
+	$scope.$parent.init("/rounds", [
+		{name:"name",width:"15em",display:"Name"},
+		{name:"date",width:"8em",display:"Date YY-MM-DD"}
+	], "date");
 });
